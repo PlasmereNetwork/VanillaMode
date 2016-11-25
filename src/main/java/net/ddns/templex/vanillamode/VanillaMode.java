ca3eafb7f63117a82fc5bd45221051f3d4473d31
VTCAKAVSMoACE <vtcakavsmoace@gmail.com>
2016-11-26 00:31:01 +0100

    Standard Bukkit plugin setup. Initial commit.

---
+++ src/main/java/net/ddns/templex/vanillamode/VanillaMode.java
@@ -0,0 +1,22 @@
+package net.ddns.templex.vanillamode;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VanillaMode extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		/* TODO:
+		 *  - Disable all other plugins.
+		 *  - Re-vanillify. This will most likely require a restart, but implementation has not been finalized.
+		 */
+	}
+	
+	@Override
+	public void onDisable() {
+		/* TODO:
+		 *  - Unload everything.
+		 */
+	}
+	
+}
